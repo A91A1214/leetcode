125aee68b7f80850d066e006f055dc69ff776369
A91A1214 <147997111+A91A1214@users.noreply.github.com>
2025-07-11 20:08:42 +0530

    Time: 4 ms (73.49%) | Memory: 45 MB (65.58%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
@@ -1,16 +1,12 @@
 class Solution {
-
     public int minOperations(int[] nums) {
-        int count = 0;
-        int currentMax = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] <= ++currentMax) {
-                count += currentMax - nums[i];
+        int count=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<=nums[i-1]){
+                count=count+nums[i-1]-nums[i]+1;
+                nums[i]=nums[i-1]+1;
             }
-            currentMax = Math.max(currentMax, nums[i]);
         }
-
         return count;
     }
 }
