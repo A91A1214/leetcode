baedb99fa67fb15b7361cce8020d549c6c057ecc
A91A1214 <147997111+A91A1214@users.noreply.github.com>
2025-10-11 04:59:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (35.81%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,38 +1,45 @@
-// class Solution {
-//     public int strStr(String haystack, String needle) {
-//         int n=needle.length();
-//         int m=haystack.length();
-//         for(int i=0;i<m-n;i++){
-//             if(haystack.equals(needle)){
-//                 return i;
-//             }
-//         }
-//         return -1;
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Solution {
     public int strStr(String haystack, String needle) {
         int n=needle.length();
-        for(int i=0;i<haystack.length()-n+1;i++){
-          if(haystack.substring(i,i+n).equals(needle)){
-            return i;
-          }
-        }
-        return -1;
+        int m=haystack.length();
+       char[] c1=haystack.toCharArray();
+       char[] c2=needle.toCharArray();
+    //    int j=0;
+       for(int i=0;i<=m-n;i++){
+         int j=0;
+        while(j<n && c1[i+j]==c2[j]){
+            j++;
+        } 
+        if(n==j) return i;
+
+       }
+      
+       return -1;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int strStr(String haystack, String needle) {
+//         int n=needle.length();
+//         for(int i=0;i<haystack.length()-n+1;i++){
+//           if(haystack.substring(i,i+n).equals(needle)){
+//             return i;
+//           }
+//         }
+//         return -1;
+//     }
+//      }
