c499ae18a3b552239856af1cb5319c338c65316d
A91A1214 <147997111+A91A1214@users.noreply.github.com>
2026-01-31 06:02:17 +0530

    Time: 10 ms (37.70%) | Memory: 49.2 MB (7.37%) - LeetSync

---
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n=nums.length;
+        int k[]=new int[n];
+        for(int i=0;i<n;i++){
+            k[i]=nums[i]*nums[i];
+        }
+        Arrays.sort(k);
+        return k;
+    }
+}
